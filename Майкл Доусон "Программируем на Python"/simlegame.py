e8cd21e3380615a993d462ad32ca9018f4d5a03b
Kurta Oleksandr <kurtasasha8@gmail.com>
2020-08-01 17:37:57 +0300

    module

---
+++ Майкл Доусон "Программируем на Python"/simlegame.py
@@ -0,0 +1,8 @@
+# Простая игра
+# Демонстрирует импорт модулей
+import games, random
+print("Добро пожаловать в самую простую игру")
+again = None
+while again != "n":
+    players = []
+    num = games.
